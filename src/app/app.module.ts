d29a5dc1ec3a478a9be9cb2e0210ae720193fe5a
Adrien BOURGON <AdrienB@acelys.local>
2018-04-20 17:33:22 +0200

    plop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import {
   MatCardModule,
   MatToolbarModule } from '@angular/material';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
 
@@ -19,7 +22,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
     BrowserModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    environment.production ? ServiceWorkerModule.register('ngssw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
